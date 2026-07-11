6cccbf7ed6fb2aba7b32d2c9ac24c82c91a25c72
Edk114 <kingedward030@gmail.com>
2026-07-11 12:07:55 +0100

    Add Android app shell with Capacitor

---
+++ android/app/src/main/java/com/athletigolf/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.athletigolf.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+const config = {
+  appId: "com.athletigolf.app",
+  appName: "AthletiGolf",
+  webDir: "dist",
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: "https",
+  },
+};
+
+export default config;
