ec9c07ff4a882c1efc49aff2b89d33da919d10fa
Edk114 <kingedward030@gmail.com>
2026-08-20 17:34:04 +0100

    refactor: modularize RoundTracker scorecard

--- src/pages/RoundTracker/components/scorecard/ScorecardControls.tsx
+++ src/pages/RoundTracker/components/scorecard/ScorecardControls.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Button } from "../../../components/ui";
+import { Button } from "@/components/ui";
 import { Hole } from "../../lib/validation";
 
 export function ScorecardControls({
